f7b551c457a7a091dc55c914e9aef4629737ae0e
gabriellimapereira <gaab.ollim@gmail.com>
2025-03-21 15:35:03 -0300

    criação dos protótipos

---
+++ QuestaoUm/prototipos.h
@@ -0,0 +1,39 @@
+#ifndef PROTOTIPOS
+#define PROTOTIPOS
+
+typedef struct InfoMusica {
+    char titulo[50];
+    int duracao;
+} InfoMusica;
+
+typedef struct ArvMusica {
+    InfoMusica info;
+    struct ArvMusica *esq, *dir;
+} ArvMusica;
+
+typedef struct InfoAlbum {
+    char titulo[50];
+    int anoLancamento;
+    int quantMusicas;
+    ArvMusica *musica;
+} InfoAlbum;
+
+typedef struct ArvAlbum {
+    InfoAlbum info;
+    struct ArvAlbum *esq, *dir;
+} ArvAlbum;
+
+typedef struct InfoArtista {
+    char nome[50];
+    char tipo[50];
+    char estilo[50];
+    int numAlbuns;
+    ArvAlbum *album;
+} InfoArtista;
+
+typedef struct ArvArtista {
+    InfoArtista info;
+    struct ArvArtista *esq, *dir;
+} ArvArtista;
+
+#endif
